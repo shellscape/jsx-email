849b9ac3d951ce3bcf0f6098c194402241763c89
lordelogos <paulehiks@gmail.com>
2023-09-27 23:58:52 +0100

    wip: add <Html> tests

--- packages/html/src/html.spec.tsx
+++ packages/html/src/html.spec.tsx
@@ -1,14 +1,26 @@
 import { render } from '@jsx-email/render';
-
 import { Html } from './index';
 
-describe('render', () => {
+describe('<Html> component', () => {
   beforeEach(() => {
     vi.restoreAllMocks();
     vi.resetModules();
   });
 
-  it('renders the <Html> component', () => {
+  it('renders children correctly', () => {
+    const testMessage = 'Test message';
+    const html = render(<Html>{testMessage}</Html>);
+    expect(html).toContain(testMessage);
+  });
+
+  it('passes props correctly', () => {
+    const html = render(<Html lang="fr" dir="rtl" data-testid="html-test" />);
+    expect(html).toContain('lang="fr"');
+    expect(html).toContain('dir="rtl"');
+    expect(html).toContain('data-testid="html-test"');
+  });
+
+  it('renders correctly', () => {
     const actualOutput = render(<Html />);
     expect(actualOutput).toMatchInlineSnapshot(
       '"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><html data-id=\\"@jsx-email/html\\" lang=\\"en\\" dir=\\"ltr\\"></html>"'
