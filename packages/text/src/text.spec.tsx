cf6c8ccce1a050192d0de9a86655c91b1df3dd9f
lordelogos <paulehiks@gmail.com>
2023-09-27 23:57:12 +0100

    wip: add <Text> tests

--- packages/text/src/text.spec.tsx
+++ packages/text/src/text.spec.tsx
@@ -1,14 +1,30 @@
 import { render } from '@jsx-email/render';
-
 import { Text } from './index';
 
-describe('render', () => {
+describe('<Text> component', () => {
   beforeEach(() => {
     vi.restoreAllMocks();
     vi.resetModules();
   });
 
-  it('renders the <Text> component', () => {
+  it('renders children correctly', () => {
+    const testMessage = 'Test message';
+    const html = render(<Text>{testMessage}</Text>);
+    expect(html).toContain(testMessage);
+  });
+
+  it('passes style and other props correctly', () => {
+    const style = { fontSize: '16px' };
+    const html = render(
+      <Text style={style} data-testid="text-test">
+        Test
+      </Text>
+    );
+    expect(html).toContain('font-size:16px');
+    expect(html).toContain('data-testid="text-test"');
+  });
+
+  it('renders correctly', () => {
     const actualOutput = render(<Text>Lorem ipsum</Text>);
     expect(actualOutput).toMatchInlineSnapshot(
       '"<!DOCTYPE html PUBLIC \\"-//W3C//DTD XHTML 1.0 Transitional//EN\\" \\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\\"><p data-id=\\"@jsx-email/text\\" style=\\"font-size:14px;line-height:24px;margin:16px 0\\">Lorem ipsum</p>"'
