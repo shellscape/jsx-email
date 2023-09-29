4a5ddc46b6e0fbb1bb8da9763def2a6c67f6e97e
lordelogos <paulehiks@gmail.com>
2023-09-29 18:46:03 +0100

    wip: add tailwind tests

--- packages/tailwind/test/tailwind.test.tsx
+++ packages/tailwind/test/tailwind.test.tsx
@@ -1,6 +1,6 @@
 import { renderToStaticMarkup as render } from 'react-dom/server';
-// import { TailwindConfig } from 'tw-to-css';
-// import { Hr } from '@jsx-email/hr';
+import { TailwindConfig } from 'tw-to-css';
+import { Hr } from '@jsx-email/hr';
 import { Head } from '@jsx-email/head';
 import { Html } from '@jsx-email/html';
 
@@ -16,335 +16,295 @@ describe('Tailwind component', () => {
     it('should render children with inline Tailwind styles', () => {
       const actualOutput = render(
         <Tailwind>
-          <Html>
-            <Head />
-            <span
-              dangerouslySetInnerHTML={{
-                __html: `<!--[if mso]><i style="letter-spacing: 10px;mso-font-width:-100%;" hidden>&nbsp;</i><![endif]-->`
-              }}
-            />
-            <div className="bg-white sm:bg-red-50 sm:text-sm md:text-lg custom-class" />
-          </Html>
+          <div className="bg-white text-sm" />
         </Tailwind>
       );
-
-      console.log(actualOutput);
 
       expect(actualOutput).not.toBeNull();
     });
   });
+
+  it('should be able to use background image', () => {
+    const actualOutput = render(
+      <Tailwind>
+        <div className="bg-[url(https://example.com/image.png)]" />
+      </Tailwind>
+    );
+
+    expect(actualOutput).toMatchSnapshot();
+  });
+
+  it('should override inline styles with Tailwind styles', () => {
+    const actualOutput = render(
+      <Tailwind>
+        <div
+          style={{ backgroundColor: 'red', fontSize: '12px' }}
+          className="bg-black text-[16px]"
+        />
+      </Tailwind>
+    );
+
+    expect(actualOutput).toMatchSnapshot();
+  });
+
+  it('should override component styles with Tailwind styles', () => {
+    const actualOutput = render(
+      <Tailwind>
+        <Hr className="w-12" />
+      </Tailwind>
+    );
+
+    expect(actualOutput).toContain('width:3rem');
+  });
 });
 
-//   it('should be able to use background image', () => {
-//     const actualOutput = render(
-//       <Tailwind>
-//         <div className="bg-[url(https://react.email/static/covers/tailwind.png)]" />
-//       </Tailwind>
-//     );
+describe('Responsive styles', () => {
+  it('should add css to <head/>', () => {
+    const actualOutput = render(
+      <Tailwind>
+        <html>
+          <head />
+          <body>
+            <div className="bg-red-200 sm:bg-red-300 md:bg-red-400 lg:bg-red-500" />
+          </body>
+        </html>
+      </Tailwind>
+    );
 
-//     expect(actualOutput).toMatchSnapshot();
-//   });
+    expect(actualOutput).toMatchSnapshot();
+  });
 
-//   it('should override inline styles with Tailwind styles', () => {
-//     const actualOutput = render(
-//       <Tailwind>
-//         <div
-//           style={{ backgroundColor: 'red', fontSize: '12px' }}
-//           className="bg-black text-[16px]"
-//         />
-//       </Tailwind>
-//     );
+  it('should throw an error when used without either <html/> or <head/> tags', () => {
+    function noHtmlOrHead() {
+      render(
+        <Tailwind>
+          <div className="bg-red-200 sm:bg-red-500" />
+        </Tailwind>
+      );
+    }
+    expect(noHtmlOrHead).toThrowErrorMatchingInlineSnapshot(
+      `"Tailwind: To use responsive styles you must have a <html> and <head> element in your template."`
+    );
 
-//     expect(actualOutput).toMatchSnapshot();
-//   });
+    function noHtml() {
+      render(
+        <Tailwind>
+          <head>
+            <title>Test</title>
+          </head>
+          <div className="bg-red-200 sm:bg-red-500" />
+        </Tailwind>
+      );
+    }
+    expect(noHtml).toThrowErrorMatchingInlineSnapshot(
+      `"Tailwind: To use responsive styles you must have a <html> and <head> element in your template."`
+    );
 
-//   it('should override component styles with Tailwind styles', () => {
-//     const actualOutput = render(
-//       <Tailwind>
-//         <Hr className="w-12" />
-//       </Tailwind>
-//     );
+    function noHead() {
+      render(
+        <Tailwind>
+          <html>
+            {/* <Head></Head> */}
+            <div className="bg-red-200 sm:bg-red-500" />
+          </html>
+        </Tailwind>
+      );
+    }
+    expect(noHead).toThrowErrorMatchingInlineSnapshot(
+      `"Tailwind: To use responsive styles you must have a <html> and <head> element in your template."`
+    );
+  });
 
-//     expect(actualOutput).toContain('width:3rem');
-//   });
-// });
+  it('should persist exsisting <head/> elements', () => {
+    const actualOutput = render(
+      <Tailwind>
+        <html>
+          <head>
+            <style />
+            <link />
+          </head>
+          <body>
+            <div className="bg-red-200 sm:bg-red-500" />
+          </body>
+        </html>
+      </Tailwind>
+    );
 
-// describe('Responsive styles', () => {
-//   it('should add css to <head/>', () => {
-//     const actualOutput = render(
-//       <Tailwind>
-//         <html>
-//           <head />
-//           <body>
-//             <div className="bg-red-200 sm:bg-red-300 md:bg-red-400 lg:bg-red-500" />
-//           </body>
-//         </html>
-//       </Tailwind>
-//     );
+    expect(actualOutput).toMatchSnapshot();
+  });
+});
 
-//     expect(actualOutput).toMatchSnapshot();
-//   });
+describe('Custom theme config', () => {
+  it('should be able to use custom colors', () => {
+    const config: TailwindConfig = {
+      theme: {
+        extend: {
+          colors: {
+            custom: '#1fb6ff'
+          }
+        }
+      }
+    };
 
-//   it('should throw an error when used without either <html/> or <head/> tags', () => {
-//     function noHtmlOrHead() {
-//       render(
-//         <Tailwind>
-//           <div className="bg-red-200 sm:bg-red-500" />
-//         </Tailwind>
-//       );
-//     }
-//     expect(noHtmlOrHead).toThrowErrorMatchingInlineSnapshot(
-//       `"Tailwind: To use responsive styles you must have a <html> and <head> element in your template."`
-//     );
+    const actualOutput = render(
+      <Tailwind config={config}>
+        <div className="text-custom bg-custom" />
+      </Tailwind>
+    );
 
-//     function noHtml() {
-//       render(
-//         <Tailwind>
-//           <head>
-//             <title>Test</title>
-//           </head>
-//           <div className="bg-red-200 sm:bg-red-500" />
-//         </Tailwind>
-//       );
-//     }
-//     expect(noHtml).toThrowErrorMatchingInlineSnapshot(
-//       `"Tailwind: To use responsive styles you must have a <html> and <head> element in your template."`
-//     );
+    expect(actualOutput).toMatchSnapshot();
+  });
 
-//     function noHead() {
-//       render(
-//         <Tailwind>
-//           <html>
-//             {/* <Head></Head> */}
-//             <div className="bg-red-200 sm:bg-red-500" />
-//           </html>
-//         </Tailwind>
-//       );
-//     }
-//     expect(noHead).toThrowErrorMatchingInlineSnapshot(
-//       `"Tailwind: To use responsive styles you must have a <html> and <head> element in your template."`
-//     );
-//   });
+  it('should be able to use custom colors', () => {
+    const config: TailwindConfig = {
+      theme: {
+        extend: {
+          fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif']
+          }
+        }
+      }
+    };
 
-//   it('should persist exsisting <head/> elements', () => {
-//     const actualOutput = render(
-//       <Tailwind>
-//         <html>
-//           <head>
-//             <style />
-//             <link />
-//           </head>
-//           <body>
-//             <div className="bg-red-200 sm:bg-red-500" />
-//           </body>
-//         </html>
-//       </Tailwind>
-//     );
+    const actualOutput = render(
+      <Tailwind config={config}>
+        <div className="font-sans" />
+        <div className="font-serif" />
+      </Tailwind>
+    );
 
-//     expect(actualOutput).toMatchSnapshot();
-//   });
-// });
+    expect(actualOutput).toMatchSnapshot();
+  });
 
-// describe('Custom theme config', () => {
-//   it('should be able to use custom colors', () => {
-//     const config: TailwindConfig = {
-//       theme: {
-//         extend: {
-//           colors: {
-//             custom: '#1fb6ff'
-//           }
-//         }
-//       }
-//     };
+  it('should be able to use custom spacing', () => {
+    const config: TailwindConfig = {
+      theme: {
+        extend: {
+          spacing: {
+            '8xl': '96rem'
+          }
+        }
+      }
+    };
+    const actualOutput = render(
+      <Tailwind config={config}>
+        <div className="m-8xl"></div>
+      </Tailwind>
+    );
+    expect(actualOutput).toMatchSnapshot();
+  });
 
-//     const actualOutput = render(
-//       <Tailwind config={config}>
-//         <div className="text-custom bg-custom" />
-//       </Tailwind>
-//     );
+  it('should be able to use custom border radius', () => {
+    const config: TailwindConfig = {
+      theme: {
+        extend: {
+          borderRadius: {
+            '4xl': '2rem'
+          }
+        }
+      }
+    };
+    const actualOutput = render(
+      <Tailwind config={config}>
+        <div className="rounded-4xl" />
+      </Tailwind>
+    );
+    expect(actualOutput).toMatchSnapshot();
+  });
 
-//     expect(actualOutput).toMatchSnapshot();
-//   });
+  it('should be able to use custom text alignment', () => {
+    const config: TailwindConfig = {
+      theme: {
+        extend: {
+          textAlign: {
+            justify: 'justify'
+          }
+        }
+      }
+    };
 
-//   it('should be able to use custom colors', () => {
-//     const config: TailwindConfig = {
-//       theme: {
-//         extend: {
-//           fontFamily: {
-//             sans: ['Graphik', 'sans-serif'],
-//             serif: ['Merriweather', 'serif']
-//           }
-//         }
-//       }
-//     };
+    const actualOutput = render(
+      <Tailwind config={config}>
+        <div className="text-justify" />
+      </Tailwind>
+    );
 
-//     const actualOutput = render(
-//       <Tailwind config={config}>
-//         <div className="font-sans" />
-//         <div className="font-serif" />
-//       </Tailwind>
-//     );
+    expect(actualOutput).toMatchSnapshot();
+  });
+});
 
-//     expect(actualOutput).toMatchSnapshot();
-//   });
+describe('Custom plugins config', () => {
+  it('should be able to use custom plugins', () => {
+    const config: TailwindConfig = {
+      plugins: [
+        ({ addUtilities }: any) => {
+          const newUtilities = {
+            '.border-custom': {
+              border: '2px solid'
+            }
+          };
 
-//   it('should be able to use custom spacing', () => {
-//     const config: TailwindConfig = {
-//       theme: {
-//         extend: {
-//           spacing: {
-//             '8xl': '96rem'
-//           }
-//         }
-//       }
-//     };
+          addUtilities(newUtilities);
+        }
+      ]
+    };
 
-//     const actualOutput = render(
-//       <Tailwind config={config}>
-//         <div className="m-8xl"></div>
-//       </Tailwind>
-//     );
+    const actualOutput = render(
+      <Tailwind config={config}>
+        <div className="border-custom" />
+      </Tailwind>
+    );
 
-//     expect(actualOutput).toMatchSnapshot();
-//   });
+    expect(actualOutput).toMatchSnapshot();
+  });
 
-//   it('should be able to use custom border radius', () => {
-//     const config: TailwindConfig = {
-//       theme: {
-//         extend: {
-//           borderRadius: {
-//             '4xl': '2rem'
-//           }
-//         }
-//       }
-//     };
+  it('should be able to use custom plugins with responsive styles', () => {
+    const config: TailwindConfig = {
+      plugins: [
+        ({ addUtilities }: any) => {
+          const newUtilities = {
+            '.border-custom': {
+              border: '2px solid'
+            }
+          };
 
-//     const actualOutput = render(
-//       <Tailwind config={config}>
-//         <div className="rounded-4xl" />
-//       </Tailwind>
-//     );
+          addUtilities(newUtilities);
+        }
+      ]
+    };
 
-//     expect(actualOutput).toMatchSnapshot();
-//   });
+    const actualOutput = render(
+      <Tailwind config={config}>
+        <html>
+          <head />
+          <body>
+            <div className="border-custom sm:border-custom" />
+          </body>
+        </html>
+      </Tailwind>
+    );
 
-//   it('should be able to use custom text alignment', () => {
-//     const config: TailwindConfig = {
-//       theme: {
-//         extend: {
-//           textAlign: {
-//             justify: 'justify'
-//           }
-//         }
-//       }
-//     };
+    expect(actualOutput).toMatchSnapshot();
+  });
+});
 
-//     const actualOutput = render(
-//       <Tailwind config={config}>
-//         <div className="text-justify" />
-//       </Tailwind>
-//     );
+describe('', () => {
+  it('should preserve mso styles', () => {
+    const actualOutput = render(
+      <Tailwind>
+        <Html>
+          <Head />
+          <span
+            dangerouslySetInnerHTML={{
+              __html: `<!--[if mso]><i style="letter-spacing: 10px;mso-font-width:-100%;" hidden>&nbsp;</i><![endif]-->`
+            }}
+          />
+          <div className="bg-white sm:bg-red-50 sm:text-sm md:text-lg custom-class" />
+        </Html>
+      </Tailwind>
+    );
 
-//     expect(actualOutput).toMatchSnapshot();
-//   });
-// });
-
-// describe('Custom plugins config', () => {
-//   it('should be able to use custom plugins', () => {
-//     const config: TailwindConfig = {
-//       plugins: [
-//         function ({ addUtilities }: any) {
-//           const newUtilities = {
-//             '.border-custom': {
-//               border: '2px solid'
-//             }
-//           };
-
-//           addUtilities(newUtilities);
-//         }
-//       ]
-//     };
-
-//     const actualOutput = render(
-//       <Tailwind config={config}>
-//         <div className="border-custom" />
-//       </Tailwind>
-//     );
-
-//     expect(actualOutput).toMatchSnapshot();
-//   });
-
-//   it('should be able to use custom plugins with responsive styles', () => {
-//     const config: TailwindConfig = {
-//       plugins: [
-//         function ({ addUtilities }: any) {
-//           const newUtilities = {
-//             '.border-custom': {
-//               border: '2px solid'
-//             }
-//           };
-
-//           addUtilities(newUtilities);
-//         }
-//       ]
-//     };
-
-//     const actualOutput = render(
-//       <Tailwind config={config}>
-//         <html>
-//           <head />
-//           <body>
-//             <div className="border-custom sm:border-custom" />
-//           </body>
-//         </html>
-//       </Tailwind>
-//     );
-
-//     expect(actualOutput).toMatchSnapshot();
-//   });
-// });
-// describe('Class name replacement', () => {
-//   it('should replace forward slash with underscore in both class name and selector', () => {
-//     const actualOutput = render(
-//       <Tailwind>
-//         <html>
-//           <head />
-//           <body>
-//             <div className="w-full" />
-//             <div className="w-1/2 sm:w-1/2" />
-//           </body>
-//         </html>
-//       </Tailwind>
-//     );
-//     expect(actualOutput).toMatchSnapshot();
-//   });
-//   it('should replace period with underscore in both class name and selector', () => {
-//     const actualOutput = render(
-//       <Tailwind>
-//         <html>
-//           <head />
-//           <body>
-//             <div className="w-1.5" />
-//             <div className="w-1.5 sm:w-1.5" />
-//           </body>
-//         </html>
-//       </Tailwind>
-//     );
-//     expect(actualOutput).toMatchSnapshot();
-//   });
-//   it('should replace percent signs with underscore in both class name and selector', () => {
-//     const actualOutput = render(
-//       <Tailwind>
-//         <html>
-//           <head />
-//           <body>
-//             <div className="w-[50%]" />
-//             <div className="w-[50%] sm:w-[50%]" />
-//           </body>
-//         </html>
-//       </Tailwind>
-//     );
-//     expect(actualOutput).toMatchSnapshot();
-//   });
-// });
-// });
+    expect(actualOutput).toMatchSnapshot();
+  });
+});
