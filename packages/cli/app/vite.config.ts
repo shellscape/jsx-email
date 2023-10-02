faaade5315429bfa367e8242bc842545e6f0cf1f
shellscape <andrew@shellscape.org>
2023-10-02 10:37:38 -0400

    fix(cli): pick up secondary changes

--- packages/cli/app/vite.config.ts
+++ packages/cli/app/vite.config.ts
@@ -5,9 +5,6 @@ import dynamicImport from 'vite-plugin-dynamic-import';
 process.chdir(__dirname);
 
 export default defineConfig({
-  build: {
-    target: 'esnext'
-  },
   define: {
     'process.env': 'import.meta.env'
   },
