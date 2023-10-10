3c9b219f2b4891bfe76861ab5fce528b3d216351
shellscape <andrew@shellscape.org>
2023-10-10 12:46:31 -0400

    fix(cli): bad Link.to, don't enforce /templates for `create`

--- packages/cli/app/src/components/sidebar.tsx
+++ packages/cli/app/src/components/sidebar.tsx
@@ -83,7 +83,7 @@ export const Sidebar = React.forwardRef<SidebarElement, Readonly<SidebarProps>>(
                     templateNames.map((item) => {
                       const isCurrentPage = title === item;
                       return (
-                        <Link key={item} href={`/${item}`}>
+                        <Link key={item} to={`/${item}`}>
                           <motion.span
                             className={classnames(
                               'text-[14px] flex items-center gap-2 w-full pl-4 h-8 rounded-md relative transition ease-in-out duration-200',
