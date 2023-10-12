525394f977a1320d26ca62875a116a332c141c9a
shellscape <andrew@shellscape.org>
2023-10-12 10:12:10 -0400

    chore(repo): add additional tests for render

--- packages/render/test/render.test.tsx
+++ packages/render/test/render.test.tsx
@@ -2,6 +2,10 @@ import { render } from '../src';
 import { Preview } from '../src/utils/preview';
 import { Template } from '../src/utils/template';
 
+import AirbnbEmail from '../../../apps/demo/emails/airbnb-review';
+import PlaidEmail from '../../../apps/demo/emails/plaid-verify-identity';
+import VercelEmail from '../../../apps/demo/staged/vercel-invite-user';
+
 describe('render', () => {
   beforeEach(() => {
     vi.restoreAllMocks();
@@ -9,21 +13,32 @@ describe('render', () => {
   });
 
   it('converts a React component into HTML', () => {
-    const actualOutput = render(<Template firstName="Jim" />);
-    expect(actualOutput).toMatchSnapshot();
+    const result = render(<Template firstName="Jim" />);
+    expect(result).toMatchSnapshot();
   });
 
   it('converts a React component into PlainText', () => {
-    const actualOutput = render(<Template firstName="Jim" />, {
-      plainText: true
-    });
-    expect(actualOutput).toMatchSnapshot();
+    const result = render(<Template firstName="Jim" />, { plainText: true });
+    expect(result).toMatchSnapshot();
   });
 
   it('converts to plain text and removes reserved ID', () => {
-    const actualOutput = render(<Preview />, {
-      plainText: true
-    });
-    expect(actualOutput).toMatchSnapshot();
+    const result = render(<Preview />, { plainText: true });
+    expect(result).toMatchSnapshot();
+  });
+
+  it('renders the airbnb demo template', () => {
+    expect(render(<AirbnbEmail />)).toMatchSnapshot();
+    expect(render(<AirbnbEmail />, { pretty: true })).toMatchSnapshot();
+  });
+
+  it('renders the plaid demo template', () => {
+    expect(render(<PlaidEmail />)).toMatchSnapshot();
+    expect(render(<PlaidEmail />, { pretty: true })).toMatchSnapshot();
+  });
+
+  it('renders the vercel demo template', () => {
+    expect(render(<VercelEmail />)).toMatchSnapshot();
+    expect(render(<VercelEmail />, { pretty: true })).toMatchSnapshot();
   });
 });
