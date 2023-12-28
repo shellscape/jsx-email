90439899b10fa8dce99635c80ec913889fca71a5
Andrew Powell <shellscape@users.noreply.github.com>
2023-12-28 13:01:48 -0500

    feat(app-preview)!: initial release (#115)

---
+++ apps/preview/app/src/components/code.tsx
@@ -0,0 +1,40 @@
+import classnames from 'classnames';
+import { getHighlighter } from 'shikiji';
+
+export type PreviewLanguage = 'html' | 'jsx' | 'plain';
+
+interface CodeProps {
+  children: string;
+  className?: string;
+  language?: PreviewLanguage;
+}
+
+const theme = 'dark-plus';
+const shiki = await getHighlighter({
+  langs: ['html', 'tsx'],
+  themes: [theme]
+});
+
+export const Code = ({ children: value, language = 'html' }: CodeProps) => {
+  const lang = language === 'jsx' ? 'tsx' : language;
+  const code = language === 'plain' ? value : shiki.codeToHtml(value, { lang, theme });
+  const lines = value.split('\n').length;
+  const css = `
+    .${language} .shiki .line:before {
+      width: calc(${lines.toString().length} * 12px + 12px);
+    }`;
+
+  return (
+    <>
+      <style>{css}</style>
+      <div
+        className={classnames(language)}
+        dangerouslySetInnerHTML={{ __html: code }}
+        style={{
+          fontFamily:
+            'SFMono-Regular,Consolas,"Liberation Mono",Menlo,Monaco,"Lucida Console","Liberation Mono","DejaVu Sans Mono","Bitstream Vera Sans Mono","Courier New"'
+        }}
+      ></div>
+    </>
+  );
+};
