4051b947119ab183113f476012abcb37a692ad45
Wlad Paiva <desenvolvedormg@gmail.com>
2023-12-29 21:49:11 -0300

    chore(repo): add preview app tests with playwright (#102)
    Co-authored-by: shellscape <andrew@shellscape.org>

---
+++ apps/test/fixtures/local-assets.tsx
@@ -0,0 +1,18 @@
+import { Body, Html, Img, Link } from 'jsx-email';
+
+export const TemplateName = 'local-assets';
+
+// @ts-ignore
+const baseUrl = import.meta.resolve('./static/');
+
+export const Template = () => {
+  const catUrl = `${baseUrl}cat.jpeg`;
+  return (
+    <Html>
+      <Body>
+        <Img id="image" src={catUrl} alt="Cat" width="200" height="200" />
+        <Link href={catUrl}>{catUrl}</Link>
+      </Body>
+    </Html>
+  );
+};
