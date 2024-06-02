dac42e2c31ee8fc44f6d6f98f6c87a1e3e57b8bf
lordelogos <paulehiks@gmail.com>
2024-06-02 20:06:59 +0100

    feat: update container component

--- packages/jsx-email/src/components/container.tsx
+++ packages/jsx-email/src/components/container.tsx
@@ -1,33 +1,52 @@
 import { debug } from '../debug';
 import type { BaseProps, JsxEmailComponent } from '../types';
 
-export interface ContainerProps extends BaseProps<'table'> {}
+export interface ContainerProps extends Omit<BaseProps<'table'>, 'align' | 'width'> {
+  alignment?: 'center' | 'left' | 'right';
+  containerWidth?: number;
+}
 
 const debugProps = debug.elements.enabled ? { dataType: 'jsx-email/container' } : {};
 
 export const Container: JsxEmailComponent<ContainerProps> = ({
+  alignment = 'center',
   children,
   disableDefaultStyle,
   style,
+  containerWidth = 600,
   ...props
 }) => (
-  <table
-    align="center"
-    width="100%"
-    {...props}
-    {...debugProps}
-    role="presentation"
-    cellSpacing="0"
-    cellPadding="0"
-    border={0}
-    style={{ ...(disableDefaultStyle ? {} : { maxWidth: '37.5em' }), ...style }}
-  >
-    <tbody>
-      <tr style={disableDefaultStyle ? {} : { width: '100%' }}>
-        <td>{children}</td>
-      </tr>
-    </tbody>
-  </table>
+  <div {...debugProps} style={{ tableLayout: 'fixed', width: '100%' }}>
+    <div style={{ margin: '0 auto', maxWidth: containerWidth }}>
+      {/* For Outlook versions < 2013: we need a table wrapper width set to `max-width` */}
+      <span
+        dangerouslySetInnerHTML={{
+          __html: `<!--[if mso]><table align=${alignment} width=${containerWidth} style="border-spacing: 0; width:${containerWidth}px;" role="presentation"><tr><td><![endif]-->`
+        }}
+      />
+      <table
+        align={alignment}
+        width="100%"
+        {...props}
+        role="presentation"
+        cellSpacing="0"
+        cellPadding="0"
+        border={0}
+        style={{ ...(disableDefaultStyle ? {} : { maxWidth: `${containerWidth}px` }), ...style }}
+      >
+        <tbody>
+          <tr style={disableDefaultStyle ? {} : { width: '100%' }}>
+            <td align={alignment}>{children}</td>
+          </tr>
+        </tbody>
+      </table>
+      <span
+        dangerouslySetInnerHTML={{
+          __html: `<!--[if mso]></td></tr></table><![endif]-->`
+        }}
+      />
+    </div>
+  </div>
 );
 
 Container.displayName = 'Container';
