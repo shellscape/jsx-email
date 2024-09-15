aeb36a650628e16813e17960a78bbe863c242c87
casungo <alessandro@casungo.top>
2024-09-15 18:16:07 +0200

    v0.0.1

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -10,7 +10,29 @@ export default defineConfig({
       social: {
         github: 'https://github.com/shellscape/jsx-email',
         discord: 'https://discord.gg/FywZN57mTg'
-      }
+      },
+      logo: {
+        replacesTitle: true,
+        src: '@assets/logo.svg'
+      },
+      editLink: {
+        baseUrl: 'https://github.com/shellscape/jsx-email/edit/main/'
+      },
+      sidebar: [
+        {
+          label: 'Getting started',
+          autogenerate: { directory: 'getting-started' }
+        },
+        {
+          label: 'Core',
+          autogenerate: { directory: 'core' }
+        },
+        {
+          label: 'Components',
+          autogenerate: { directory: 'components' }
+        }
+      ],
+      credits: true
     })
   ]
 });
