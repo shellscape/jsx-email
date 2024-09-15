3f9630d8b88a37c60dd27a143540e7ce9827f084
casungo <alessandro@casungo.top>
2024-09-15 17:13:51 +0200

    initial creation

---
+++ apps/docs/src/content/config.ts
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-unresolved */
+import { defineCollection } from 'astro:content';
+import { docsSchema } from '@astrojs/starlight/schema';
+
+export const collections = {
+  docs: defineCollection({ schema: docsSchema() })
+};
