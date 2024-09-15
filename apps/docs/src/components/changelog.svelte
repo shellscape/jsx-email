48576f3260d6c9e5a419982c4c47631f77cb47fa
casungo <alessandro@casungo.top>
2024-09-16 01:02:46 +0200

    Update changelog.svelte

--- apps/docs/src/components/changelog.svelte
+++ apps/docs/src/components/changelog.svelte
@@ -19,7 +19,6 @@
 </script>
 
 <div class="bg-base-200 p-6 rounded-lg">
-  <h2 class="text-2xl font-bold mb-4">Changelog</h2>
   <p class="mb-4">Explore the latest contributions to jsx-email</p>
   <a
     href="https://github.com/shellscape/jsx-email/pulls?q=is%3Apr+is%3Aclosed"
@@ -34,28 +33,55 @@
     <ul class="timeline timeline-vertical">
       {#each changelogItems as item, index}
         <li>
-          <div class="timeline-start">{new Date(item.date).toLocaleDateString()}</div>
-          <div class="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="w-5 h-5"
-              ><path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clip-rule="evenodd"
-              /></svg
-            >
-          </div>
-          <div class="timeline-end timeline-box">
-            <div class="flex items-center mb-2">
-              <img src={item.avatar} alt={item.author} class="w-6 h-6 rounded-full mr-2" />
-              <span class="font-semibold">{item.author}</span>
+          {#if index % 2 === 0}
+            <div class="timeline-start timeline-box">
+              <div class="flex items-center mb-2">
+                <img src={item.avatar} alt={item.author} class="w-6 h-6 rounded-full mr-2" />
+                <span class="font-semibold">{item.author}</span>
+              </div>
+              <p class="text-sm">{item.description}</p>
+              <a href={item.url} class="text-xs text-primary">#{item.number}</a>
             </div>
-            <p class="text-sm">{item.description}</p>
-            <a href={item.url} class="text-xs text-primary">#{item.number}</a>
-          </div>
+            <div class="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+            <div class="timeline-end">{new Date(item.date).toLocaleDateString()}</div>
+          {:else}
+            <div class="timeline-start">{new Date(item.date).toLocaleDateString()}</div>
+            <div class="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+            <div class="timeline-end timeline-box">
+              <div class="flex items-center mb-2">
+                <img src={item.avatar} alt={item.author} class="w-6 h-6 rounded-full mr-2" />
+                <span class="font-semibold">{item.author}</span>
+              </div>
+              <p class="text-sm">{item.description}</p>
+              <a href={item.url} class="text-xs text-primary">#{item.number}</a>
+            </div>
+          {/if}
           {#if index !== changelogItems.length - 1}
             <hr />
           {/if}
