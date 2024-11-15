eb50c5ad9a5a9db12bafdbc875df8291bd162d20
shellscape <andrew@shellscape.org>
2024-11-15 18:12:49 -0500

    chore: fix tests

--- test/cli/create-jsx-email.test.ts
+++ test/cli/create-jsx-email.test.ts
@@ -15,14 +15,17 @@ describe('create-jsx-email', async () => {
       // Note: For some reason `pnpm exec` is fucking with our CWD, and resets it to
       // packages/jsx-email, which causes the config not to be found. so we use npx instead
     })`IS_CLI_TEST=true create-jsx-email .test/new --yes`;
-    const plain = strip(stdout).replace(/^(.*)create-jsx-email/, 'create-jsx-email');
+    const plain = strip(stdout)
+      .replace(/^(.*)create-jsx-email/, 'create-jsx-email')
+      .replace(/v(\d+\.\d+\.\d+)/, '');
 
     expect(plain).toMatchSnapshot();
 
     const contents = await readFile(join(__dirname, '.test/new/templates/email.tsx'), 'utf8');
     expect(contents).toMatchSnapshot();
 
-    const files = await globby('.test/new/**/*');
+    const files = await globby('.test/new/**/*', { dot: true });
+
     expect(files).toMatchSnapshot();
   });
 });
