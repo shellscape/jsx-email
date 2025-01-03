5f53265273619f95bae76ad3207a98c102475c8b
Paul Ehikhuemen <paulehiks@gmail.com>
2025-01-03 17:37:18 +0100

    wip: update isValidElement implementation

--- packages/jsx-email/src/renderer/is-valid-element.ts
+++ packages/jsx-email/src/renderer/is-valid-element.ts
@@ -1,4 +1,4 @@
-export const REACT_ELEMENT_TYPE: symbol = Symbol.for('react.element');
+// export const REACT_ELEMENT_TYPE: symbol = Symbol.for('react.element');
 // const REACT_ELEMENT_TYPE = Symbol.for('react.element');
 
 interface ReactElementLike {
