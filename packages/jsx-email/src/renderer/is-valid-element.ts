def856e842b17dfcf6d5a816aa4e5960d0198ce8
Paul Ehikhuemen <paulehiks@gmail.com>
2025-01-03 17:36:47 +0100

    wip: update isValidElement implementation

--- packages/jsx-email/src/renderer/is-valid-element.ts
+++ packages/jsx-email/src/renderer/is-valid-element.ts
@@ -1,8 +1,5 @@
-/**
-  src: https://github.com/facebook/react/blob/3b009b4cd58d382cfde2a72232c87df1a34d56d5/packages/react/src/jsx/ReactJSXElement.js#L993
- */
-
-const REACT_ELEMENT_TYPE = Symbol.for('react.element');
+export const REACT_ELEMENT_TYPE: symbol = Symbol.for('react.element');
+// const REACT_ELEMENT_TYPE = Symbol.for('react.element');
 
 interface ReactElementLike {
   $$typeof: symbol;
@@ -16,6 +13,6 @@ export function isValidElement(object: unknown): object is ReactElementLike {
   return (
     typeof object === 'object' &&
     object !== null &&
-    (object as ReactElementLike).$$typeof === REACT_ELEMENT_TYPE
+    typeof (object as ReactElementLike).$$typeof === 'symbol'
   );
 }
